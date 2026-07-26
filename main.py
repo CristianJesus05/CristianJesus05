add5dde09ecf6789e4f3734e213873dde7bfcf86
CristianJesus05 <roblescotes123@gmail.com>
2026-07-26 13:06:00 -0500

    Agregando funcion saludar ala rama secundaria

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 print("¡Hola! Practicando Git y Python desde la terminal.")
+def saludar(nombre):
+    return f"¡Hola, {nombre}! Bienvenido a la rama de desarrollo."
+
+print("¡Hola! Practicando Git y Python desde la terminal.")
+print(saludar("Cristian"))
